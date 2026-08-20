3f2c9b177f802cb6f41b05d9f5a40c3ea929024c
adittiadwikurniawan <adittiadwikurniawan@gmail.com>
2026-08-20 23:50:32 +0700

    Merubah Sesuai Work

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,12 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send('Backend Express (TypeScript) Berhasil Berjalan!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server Express berjalan di http://localhost:${port}`);
 });
